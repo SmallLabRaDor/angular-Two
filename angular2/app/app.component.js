c368543e06244b8e023c646f209edf25bef8be95
SmallLabRaDor <745453570@qq.com>
2017-09-19 10:15:38 +0800

    angular2

---
+++ angular2/app/app.component.js
@@ -0,0 +1,10 @@
+﻿(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'my-app',
+      template: '<h1>只有不断的充实自己，才可以立于不败之地</h1>'
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
